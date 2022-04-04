ef8cc093ebf86039215e13a7ffb4f2eb475db164
Bill <zloop1995@gmail.com>
2022-04-03 20:12:12 -0500

    New views

---
+++ app/src/main/java/com/example/pry2021271/AdminProfileEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.pry2021271;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminProfileEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_profile_edit);
+    }
+}
